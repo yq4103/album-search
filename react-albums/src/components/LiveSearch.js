85767d08d91d19f559796a3780355f02c7615763
Yirong Qin <yq4103@mun.ca>
2021-09-30 18:55:22 -0400

    learing axios

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useState } from "react";
-
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
 
@@ -7,6 +7,13 @@ export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
 
+  useEffect(() => {
+    const testURL = `https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`;
+    axios.get(testURL).then(response => {
+      setResults([...response.data.results])
+    });
+  }, [term]);
+  
   return (
     <Fragment>
       <header className="logo">
